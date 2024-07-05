d90db114d3fc2d2b2adf22a540e28d22e2b7da4a
ViniciusPalharess <depaulapalharesv@gmail.com>
2024-07-05 08:18:32 -0300

    criação da categoria, acessorio, cor e marca

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet
 
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
+router.register(r"acessorios", AcessorioViewSet, basename="acessorio") 
+router.register(r"categoria", CategoriaViewSet, basename="categoria") 
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
